293cb77f3ac32f0a6717220a194be5a11c2f5e45
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 17:55:10 +0800

    test: expect QuantaPDF 2.25.0

--- tests/test_version.c
+++ tests/test_version.c
@@ -3,7 +3,7 @@
 #if QUANTAPDF_VERSION_MAJOR != 2
 #error unexpected major version
 #endif
-#if QUANTAPDF_VERSION_MINOR != 24
+#if QUANTAPDF_VERSION_MINOR != 25
 #error unexpected minor version
 #endif
 #if QUANTAPDF_VERSION_PATCH != 0
