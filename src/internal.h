aad7d4fdf52c09529f8746f8da28e19790d06ec4
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 11:56:36 +0800

    refactor(composer): track reusable form PDF version

--- src/internal.h
+++ src/internal.h
@@ -189,6 +189,7 @@ typedef struct quantapdf_composer_form_state {
     size_t pdf_size;
     float width_points;
     float height_points;
+    int requires_pdf_16;
 } quantapdf_composer_form_state;
 
 typedef struct quantapdf_composer_operation {
